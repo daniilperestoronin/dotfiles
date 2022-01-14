7a670dfae23f5b4deb46248c3992ef106a07991b
daniilperestoronin <perestoronin.daniil@gmail.com>
2022-01-15 01:01:44 +0300

    Update bootsrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,20 @@ function doIt() {
 		-avh --no-perms . ~;
 }
 
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+read -p "Are you what to start components setup? (y/n)" -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	sh setup.sh
+fi
+
+read -p "Are you whant to copy dotfiles in your home directory(This may overwrite existing files)? (y/n)" -n 1;
 echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	doIt;
+fi
+
+read -p "Are you what to update macOS settings? (y/n)" -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	sh .macos
 fi

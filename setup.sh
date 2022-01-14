9283bda38a0699d632ec73edfc1c5b6c38618a7f
daniilperestoronin <perestoronin.daniil@gmail.com>
2022-01-14 18:02:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Hombrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Homebrew has installed successfully"
 else
     echo "Updating Homebrew"
@@ -31,23 +33,35 @@ brew install openssh
 # Install git
 brew install git
 
+# Install terminal things
 brew install --cask alacritty
 brew install tmux
 
+# Install programming languages
 brew install openjdk@11
 brew install python@3.9
 brew install go@1.16
 brew install node@17
 
+brew install yarn
+brew install npm
+
+# Install virtualization and cloud things
 brew install --cask docker
 brew install kubectl
 brew install helm
 brew install --cask google-cloud-sdk
 brew install awscli
+# Install lens
+brew install --cask lens
 
-brew install yarn
-brew install npm
+# Install API platforms
+# Install postman
+brew install --cask postman
+# Install bloomrpc
+brew install --cask bloomrpc
 
+# Install text editors and ide
 # Install nvim
 brew install neovim
 # Install VSC
@@ -57,15 +71,23 @@ brew install --cask intellij-idea
 
 # Install telegram
 brew install --cask telegram-desktop
+# Install zoom
+brew install --cask zoom
+
+# Install chromium
+brew install --cask chromium
+
+# Plugins plugins
 
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
+wait
 
-git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/themes}/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -73,4 +95,3 @@ cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
-

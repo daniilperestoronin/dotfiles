832666f63484ee111170c0482d9b1068207eeee0
daniil.perestoronin <perestoronin.daniil@gmail.com>
2022-04-23 22:36:28 +0300

    Refactor nvim conf

---
+++ .config/nvim/lua/user/cmp.lua
@@ -0,0 +1,85 @@
+local kind_icons = {
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+}
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then return end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then return end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+cmp.setup({
+    snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end
+    },
+    sources = {{name = "nvim_lsp"}, {name = "luasnip"}},
+    formatting = {
+        fields = {"abbr", "kind", "menu"},
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s  %s", kind_icons[vim_item.kind],
+                                          vim_item.kind)
+            vim_item.menu = ({
+                nvim_lsp = "LSP",
+                luasnip = "Snippet",
+                buffer = "Buffer",
+                path = "Path"
+            })[entry.source.name]
+            return vim_item
+        end
+    }
+})
